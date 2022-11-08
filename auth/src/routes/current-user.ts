008296e85472b33193f515ff87a68db06104c348
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-09 11:41:58 +1300

    current user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
 router.get('/api/users/currentuser', (req, res) => {
-  res.json({ msg: 'current user route' });
+  if (!req.session?.jwt) {
+    return res.json({ currentUser: null });
+  }
+  try {
+    const currentUser = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.json({ currentUser: currentUser });
+  } catch (error) {
+    return res.json({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
