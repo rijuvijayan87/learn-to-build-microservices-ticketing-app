0fb3f516e0512182a7f4f347ce034d9e65af0c82
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-09 11:20:06 +1300

    feat: signin route

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
 import { validateRequest } from '../middleware/validate-request';
-
+import { User } from '../models/user';
+import { Password } from '../services/password';
+import jwt from 'jsonwebtoken';
 const router = express.Router();
 
 router.post(
@@ -14,8 +17,35 @@ router.post(
       .withMessage('user password is missing in the request body'),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const passwordMatch = await Password.compare(
+      existingUser.password,
+      password
+    );
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid credentials');
+    }
+    // Generate jwt
+    const userJwt = jwt.sign(
+      {
+        id: existingUser.id,
+        email: existingUser.email,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // store it in session object
+    req.session = {
+      jwt: userJwt,
+    };
+
+    res.status(200).json(existingUser);
   }
 );
 
