f3d7de9688f6a0cafc2e1d8be2e3a0374bbf3afc
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-09 14:32:23 +1300

    chore: code refactor as a prep for writing tests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,7 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middleware/error-handler';
-import { RouteNotFoundError } from './errors/route-not-found-error';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
+import { app } from './app';
 
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
 const PORT = process.env.PORT || 3000;
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res) => {
-  throw new RouteNotFoundError();
-});
-
-app.use(errorHandler);
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
