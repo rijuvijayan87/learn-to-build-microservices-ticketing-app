ed23d2e89d2e2db6b17746173ba809f8f83ac4cc
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-13 21:36:40 +1300

    chore: build axios client to handle base url

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
     // console.log(currentUser);
@@ -9,21 +9,11 @@ const LandingPage = ({ currentUser }) => {
     return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-    console.log(req.headers);
-    if (typeof window === 'undefined') {
-        // we are on server. so the requests needs to be sent to http://SERVICENAME.NAMESPACE.svc.cluster.local
-        const { data } = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-            headers: req.headers,
-        });
-        console.log(`I AM IN NODE SERVER AND USER DATA IS : ${data.currentUser}`);
-        return data;
-    } else {
-        // we are on browser and requests can be made with a base url of ''
-        console.log('I AM IN THE BROWSER');
-        const { data } = await axios.get('/api/users/currentuser');
-        return data;
-    }
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentuser');
+
+    return data;
 };
 
 export default LandingPage;
