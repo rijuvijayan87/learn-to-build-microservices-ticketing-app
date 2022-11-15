d8379883daf191ef718ff24fdde0c120612c791a
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-15 19:25:30 +1300

    chore: merge conflict resolved

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -5,8 +5,7 @@ import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler } from './middleware/error-handler';
-import { RouteNotFoundError } from './errors/route-not-found-error';
+import { errorHandler, RouteNotFoundError } from '@ticketing-rv/common';
 import cookieSession from 'cookie-session';
 
 const app = express();
