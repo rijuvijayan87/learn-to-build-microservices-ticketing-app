adc81e8d285ad0a395c96c22219edb8e97872590
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-15 23:09:38 +1300

    chore: tickets and auth mongo depl refactor

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,9 +7,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
   try {
-    await mongoose.connect('mongodb://auth-mongo-service:27017/auth');
-    console.log('Connected to Mongodb..');
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to auth mongodb..');
   } catch (error) {
     console.error(error);
   }
