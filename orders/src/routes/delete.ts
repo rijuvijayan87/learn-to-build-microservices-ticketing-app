c94b6b53e4b1e48378062ecf699c1b5e931f1697
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-22 12:07:44 +1300

    feat: delete order route and tests

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,31 @@
+import {
+  NotAuthorizedError,
+  OrderStatus,
+  ResourceNotFoundError,
+} from '@ticketing-rv/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
+  const { orderId } = req.params;
+
+  const order = await Order.findById(orderId);
+  if (!order) {
+    throw new ResourceNotFoundError();
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+
+  // publish an event saying that this order was cancelled
+
+  res.status(204).json(order);
 });
 
 export { router as deleteOrderRouter };
