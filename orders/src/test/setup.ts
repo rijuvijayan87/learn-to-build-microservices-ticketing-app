070fcfbe607ec6b305a582bd7340eab60d3a9e68
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-22 11:42:05 +1300

    feat: index.ts tests

---
+++ orders/src/test/setup.ts
@@ -0,0 +1,43 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+declare global {
+  var signin: () => string;
+}
+
+jest.mock('../nats-wrapper');
+
+let mongo: any;
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'asdfasdf';
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+  collections.map((collection) => collection.deleteMany({}));
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+global.signin = () => {
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
+  };
+  const signed = {
+    jwt: jwt.sign(payload, process.env.JWT_KEY!),
+  };
+  const base64 = Buffer.from(JSON.stringify(signed)).toString('base64');
+  return `session= ${base64}`;
+};
