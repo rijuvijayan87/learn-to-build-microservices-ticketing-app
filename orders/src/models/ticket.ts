4601209a43d7749607314ed5ac7d618a7c08c41b
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-21 15:57:43 +1300

    chore: order service schema setup

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,47 @@
+import mongoose, { mongo } from 'mongoose';
+
+interface TicketAttributes {
+  title: string;
+  price: number;
+}
+
+export interface TicketDocument extends mongoose.Document {
+  title: string;
+  price: number;
+}
+
+interface TicketModel extends mongoose.Model<TicketDocument> {
+  build(attrs: TicketAttributes): TicketDocument;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        (ret.id = ret._id), delete ret._id;
+      },
+    },
+  }
+);
+
+ticketSchema.statics.build = (attrs: TicketAttributes) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDocument, TicketModel>(
+  'Ticket',
+  ticketSchema
+);
+
+export { Ticket };
