ab5731dcef65043993526fb4dcea35595877310c
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-22 11:54:47 +1300

    feat: show route handler - order service

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,9 +1,23 @@
+import {
+  NotAuthorizedError,
+  ResourceNotFoundError,
+} from '@ticketing-rv/common';
 import express, { Request, Response } from 'express';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
 router.get('/api/orders/:orderId', async (req: Request, res: Response) => {
-  res.send({});
+  const order = await Order.findById(req.params.orderId).populate('ticket');
+
+  if (!order) {
+    throw new ResourceNotFoundError();
+  }
+
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+  res.send(order);
 });
 
 export { router as showOrderRouter };
