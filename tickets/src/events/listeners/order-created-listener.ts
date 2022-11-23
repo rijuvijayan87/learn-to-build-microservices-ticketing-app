14d37971aea2790f65bfa0aaa6f0ba3b0c8b0e0a
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-23 18:49:49 +1300

    test: order created listener test

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -5,8 +5,8 @@ import {
 } from '@ticketing-rv/common';
 import { Message } from 'node-nats-streaming';
 import { Ticket } from '../../models/ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 import { queueGroupName } from './order-queuegroup-name';
-
 export class OrderCreatedListener extends EventListener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
@@ -24,6 +24,14 @@ export class OrderCreatedListener extends EventListener<OrderCreatedEvent> {
     // save
     await ticket.save();
     // ack message
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
     msg.ack();
   }
 }
