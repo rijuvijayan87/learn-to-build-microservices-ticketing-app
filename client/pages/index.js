c7828ea4f48525dfbf387764354be1447b64e4cf
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-24 19:04:48 +1300

    feat: adding links to tickets

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,15 @@
-
+import Link from "next/link";
 const LandingPage = ({ currentUser, tickets }) => {
     const ticketList = tickets.map(ticket => {
         return (
             <tr key={ticket.id}>
                 <td>{ticket.title}</td>
                 <td>{ticket.price}</td>
+                <td>
+                    <Link legacyBehavior href="/ticket/[ticketId]" as={`/ticket/${ticket.id}`}>
+                        <a>View</a>
+                    </Link>
+                </td>
             </tr>
         );
     });
@@ -17,6 +22,7 @@ const LandingPage = ({ currentUser, tickets }) => {
                     <tr>
                         <th>Title</th>
                         <th>Price</th>
+                        <th>Link</th>
                     </tr>
                 </thead>
                 <tbody>
