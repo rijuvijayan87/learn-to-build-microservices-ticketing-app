235e0edc4e7ac7ca24d0b245717df95d92cbcd01
Riju Vijayan <riju.vijayan@digio.com.au>
2022-11-24 22:46:50 +1300

    feat: complete ui code

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,3 +1,4 @@
+import Router from "next/router";
 import { useEffect, useState } from "react";
 import StripeCheckout from 'react-stripe-checkout';
 import useRequest from "../../hooks/useRequest";
@@ -12,7 +13,7 @@ const OrderShow = ({ order, currentUser }) => {
         body: {
             orderId: order.id
         },
-        onSuccess: (payment) => console.log(payment)
+        onSuccess: () => Router.push('/orders')
     });
 
     useEffect(() => {
